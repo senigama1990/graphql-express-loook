518cc221276c19c714ddc67242e555f72a256bc8
senigama1990 <senigama1990@mail.ru>
2022-03-09 19:33:29 +0500

    made graphql express server

--- src/modules/resolvers.js
+++ src/modules/resolvers.js
@@ -1,9 +1,75 @@
-const resolvers = {
-  Query: {
-    user: () => "Is working"
-  }
-}
+import { read, write } from "../lib/orm.js";
 
-export {
-  resolvers
-}
+const resolvers = {
+  Mutation: {
+    addUser: (_, user) => {
+      let users = read("users");
+      user.user_id = users.length ? users[users.length - 1].user_id + 1 : 1;
+      users.push(user);
+      if (write("users", users)) {
+        return {
+          status: 201,
+          message: "The user added",
+          data: user,
+        };
+      }
+    },
+
+    addOrder: (_, {foodId, userId, count}) => {
+      let orders = read("orders");
+      let order = orders.find(order => order.food_id == foodId && order.user_id == userId)
+      if (order) {
+        order.count = +order.count + +count
+      } else {
+        order = {
+          order_id: orders.length ? orders[orders.length - 1].order_id + 1 : 1,
+          user_id: userId,
+          food_id: foodId,
+          count
+        }
+        orders.push(order)
+      }
+      if (write("orders", orders)) {
+        return {
+          status: 201,
+          message: "The order added or updated",
+          data: order
+        }
+      }
+    },
+  },
+
+  // ______________________________________________________________________
+  Query: {
+    users: () => read("users"),
+    orders: (_, { userId }) =>
+      !userId
+        ? read("orders")
+        : read("orders").filter((order) => order.user_id == userId),
+    foods: () => read("foods"),
+  },
+
+  User: {
+    userId: (global) => global.user_id,
+    orders: (global) => {
+      let orders = read("orders");
+      return orders.filter((order) => order.user_id == global.user_id);
+    },
+  },
+
+  Order: {
+    orderId: (global) => global.order_id,
+    food: (global) =>
+      read("foods").find((food) => food.food_id == global.food_id),
+    user: (global) =>
+      read("users").find((user) => user.user_id == global.user_id),
+  },
+
+  Food: {
+    foodId: (global) => global.food_id,
+    foodName: (global) => global.food_name,
+    foodImg: (global) => global.food_img,
+  },
+};
+
+export { resolvers };
--- src/modules/typeDevs.js
+++ src/modules/typeDevs.js
@@ -2,10 +2,45 @@ import { gql } from 'apollo-server-express';
 
 
 const typeDefs = gql`
-  type Query {
-    user: String!
+  scalar Any
+
+  type Mutation {
+    addUser(username: String!, contact: String!): MutationResponse
+    addOrder(foodId: Int!, userId: Int!, count: Int!): MutationResponse
   }
-`
+
+  type MutationResponse {
+    status: Int!
+    message: String!
+    data: Any
+  }
+
+  type Query {
+    users: [User!]!
+    orders(userId: ID): [Order!]!
+    foods: [Food!]!
+  }
+
+  type User {
+    userId: ID!
+    username: String!
+    contact: String!
+    orders: [Order!]!
+  }
+
+  type Order {
+    orderId: ID!
+    food: Food!
+    user: User!
+    count: Int!
+  }
+
+  type Food {
+    foodId: ID!
+    foodName: String!
+    foodImg: String!
+  }
+`;
 
 export {
   typeDefs
